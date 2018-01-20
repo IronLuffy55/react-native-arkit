3ebdd781b666bb37fd7e9f7e452ef616df240fb9
wil <interianoe55@gmail.com>
2018-01-20 18:33:31 -0500

    Removed trailiing commas; Fixed color to be number type not string

--- components/lib/propTypes.js
+++ components/lib/propTypes.js
@@ -1,62 +1,62 @@
-import { NativeModules } from 'react-native';
-import { values } from 'lodash';
-import PropTypes from 'prop-types';
+import { NativeModules } from "react-native";
+import { values } from "lodash";
+import PropTypes from "prop-types";
 
 const ARKitManager = NativeModules.ARKitManager;
 
 export const position = PropTypes.shape({
   x: PropTypes.number,
   y: PropTypes.number,
-  z: PropTypes.number,
+  z: PropTypes.number
 });
 
 export const scale = PropTypes.number;
 export const categoryBitMask = PropTypes.number;
 export const transition = PropTypes.shape({
-  duration: PropTypes.number,
+  duration: PropTypes.number
 });
 export const eulerAngles = PropTypes.shape({
   x: PropTypes.number,
   y: PropTypes.number,
-  z: PropTypes.number,
+  z: PropTypes.number
 });
 
 export const rotation = PropTypes.shape({
   x: PropTypes.number,
   y: PropTypes.number,
   z: PropTypes.number,
-  w: PropTypes.number,
+  w: PropTypes.number
 });
 
 export const orientation = PropTypes.shape({
   x: PropTypes.number,
   y: PropTypes.number,
   z: PropTypes.number,
-  w: PropTypes.number,
+  w: PropTypes.number
 });
 
 export const shaders = PropTypes.shape({
   [ARKitManager.ShaderModifierEntryPoint.Geometry]: PropTypes.string,
   [ARKitManager.ShaderModifierEntryPoint.Surface]: PropTypes.string,
   [ARKitManager.ShaderModifierEntryPoint.LightingModel]: PropTypes.string,
-  [ARKitManager.ShaderModifierEntryPoint.Fragment]: PropTypes.string,
+  [ARKitManager.ShaderModifierEntryPoint.Fragment]: PropTypes.string
 });
 
 export const lightingModel = PropTypes.oneOf(
-  values(ARKitManager.LightingModel),
+  values(ARKitManager.LightingModel)
 );
 
 export const castsShadow = PropTypes.bool;
 export const renderingOrder = PropTypes.number;
 export const blendMode = PropTypes.oneOf(values(ARKitManager.BlendMode));
 export const chamferMode = PropTypes.oneOf(values(ARKitManager.ChamferMode));
-export const color = PropTypes.string;
+export const color = PropTypes.number;
 export const fillMode = PropTypes.oneOf(values(ARKitManager.FillMode));
 
 export const lightType = PropTypes.oneOf(values(ARKitManager.LightType));
 export const shadowMode = PropTypes.oneOf(values(ARKitManager.ShadowMode));
 export const colorBufferWriteMask = PropTypes.oneOf(
-  values(ARKitManager.ColorMask),
+  values(ARKitManager.ColorMask)
 );
 
 export const opacity = PropTypes.number;
@@ -64,7 +64,7 @@ export const opacity = PropTypes.number;
 export const materialProperty = PropTypes.shape({
   path: PropTypes.string,
   color: PropTypes.string,
-  intensity: PropTypes.number,
+  intensity: PropTypes.number
 });
 
 export const material = PropTypes.shape({
@@ -83,5 +83,5 @@ export const material = PropTypes.shape({
   doubleSided: PropTypes.bool,
   litPerPixel: PropTypes.bool,
   transparency: PropTypes.number,
-  fillMode,
+  fillMode
 });
